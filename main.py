31c71a98ad8a3ba81c3656c5fe1fd21bfd7640a5
armasuv <armasuv@educ.college-montreal.qc.ca>
2023-10-11 10:24:54 -0400

    black oogay

--- main.py
+++ main.py
@@ -2,42 +2,57 @@
 # Crée par Victor-Ionut Armasu
 
 #importation de la fonction random
-import random
 
-#On définit les variables
-nombre_essaie = 0
-question = True
+# Fonction jeu qui contient le code
+def jeu():
+    repete = True
+    # boucle while qui repète le jeu
+    while repete:
+        # fonction qui import random
+        import random
 
-#On choisis un nombre aléatoire
+        #On définit les variables
+        nombre_essaie = 0
+        question = True
 
-nbr1 = int(input("Entrez un premier nombre:"))
-nbr2 = int(input("Entrez un deuxième nombre:"))
+        #On choisis un nombre aléatoire
 
-nombre_aleatoire = random.randint(nbr1, nbr2)
+        nbr1 = int(input("Entrez un premier nombre:"))
+        nbr2 = int(input("Entrez un deuxième nombre:"))
 
-#Dit le contexte
-print("J’ai choisi un nombre au hasard entre %d et %d. \n À vous de le deviner..." % (nbr1, nbr2))
+        nombre_aleatoire = random.randint(nbr1, nbr2)
 
-#On utilise la fonction while pour poser la question
+        #Dit le contexte
+        print("J’ai choisi un nombre au hasard entre %d et %d. \n À vous de le deviner..." % (nbr1, nbr2))
 
-while question:
-    essaie = int(input("Entrez votre essai :_"))
+        #On utilise la fonction while pour poser la question
 
-    #Si c'est bon ça fini la question
+        while question:
+            essaie = int(input("Entrez votre essai :_"))
 
-    if essaie == nombre_aleatoire:
-        print("Bravo, vous avez deviné en %d essais" % nombre_essaie)
-        question = False
+            #Si c'est bon ça fini la question
 
-    #sinon ça dit que c'est plus grand que l'Essaie
+            if essaie == nombre_aleatoire:
+                nombre_essaie += 1
+                print("Bravo, vous avez deviné en %d essais" % nombre_essaie)
+                question = False
+                # Question qui demande si l'utilisateur veut refaire le programme
+                refaire = str(input('Voulez vous recommencer?'))
+                if refaire == 'oui':
+                    repete = True
+                else:
+                    print('Bye')
+                    repete = False
+            #sinon ça dit que c'est plus grand que l'Essaie
 
-    elif essaie < nombre_aleatoire:
-        print("Mauvais choix, le nombre est plus grand que %d" %essaie)
-        nombre_essaie += 1
+            elif essaie < nombre_aleatoire:
+                print("Mauvais choix, le nombre est plus grand que %d" %essaie)
+                nombre_essaie += 1
 
-    #sinon ça dit que c'est plus petit que l'essaie
+            #sinon ça dit que c'est plus petit que l'essaie
 
-    elif essaie > nombre_aleatoire:
-        print("Mauvaise réponse, le nombre est plus petit que %d" %essaie)
-        nombre_essaie += 1
-
+            elif essaie > nombre_aleatoire:
+                print("Mauvaise réponse, le nombre est plus petit que %d" %essaie)
+                nombre_essaie += 1
+# Ligne qui active la fonction jeu
+jeu()
