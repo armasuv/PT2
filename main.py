916c5f5dd18ab06d23aa883f6ee555994976b459
armasuv <armasuv@educ.college-montreal.qc.ca>
2023-10-20 10:06:18 -0400

    hello world

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # TP2
-# Crée par Victor-Ionut Armasu
+# Crée par Victor-Ionut Armasu, 2023
 
 #importation de la fonction random
+import random
 
-# Fonction jeu qui contient le code
 def jeu():
     repete = True
     # boucle while qui repète le jeu
     while repete:
-        # fonction qui import random
-        import random
 
         #On définit les variables
         nombre_essaie = 0
@@ -29,30 +27,28 @@ def jeu():
 
         while question:
             essaie = int(input("Entrez votre essai :_"))
-
+            nombre_essaie += 1
             #Si c'est bon ça fini la question
 
             if essaie == nombre_aleatoire:
-                nombre_essaie += 1
+
                 print("Bravo, vous avez deviné en %d essais" % nombre_essaie)
                 question = False
                 # Question qui demande si l'utilisateur veut refaire le programme
                 refaire = str(input('Voulez vous recommencer?'))
-                if refaire == 'oui':
-                    repete = True
-                else:
-                    print('Bye')
+                if refaire == 'non':
                     repete = False
+
             #sinon ça dit que c'est plus grand que l'Essaie
 
             elif essaie < nombre_aleatoire:
                 print("Mauvais choix, le nombre est plus grand que %d" %essaie)
-                nombre_essaie += 1
+
 
             #sinon ça dit que c'est plus petit que l'essaie
 
             elif essaie > nombre_aleatoire:
                 print("Mauvaise réponse, le nombre est plus petit que %d" %essaie)
-                nombre_essaie += 1
+
 # Ligne qui active la fonction jeu
 jeu()
